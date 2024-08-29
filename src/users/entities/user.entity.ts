41239347b9bb2a91e8c6f6fd807283312731e170
laptop <107790477+mihail-fomin@users.noreply.github.com>
2024-08-29 09:49:38 +0500

    Added userr schema

---
+++ src/users/entities/user.entity.ts
@@ -0,0 +1,21 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { User } from '@prisma/client';
+
+export class UserEntity implements User {
+  @ApiProperty()
+  id: string;
+
+  @ApiProperty()
+  createdAt: Date;
+
+  @ApiProperty()
+  updatedAt: Date;
+
+  @ApiProperty()
+  name: string;
+
+  @ApiProperty()
+  email: string;
+
+  password: string;
+}
